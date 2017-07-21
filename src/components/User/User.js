dd7c1460c6535b9efcc23d089497daf0c1d1faaf
skyle0115 <skyle0115@gmail.com>
2017-07-21 16:59:38 +0800

    edit self intro button

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -17,7 +17,8 @@ export default class User extends Component {
                 <div className="user-author">
                     <img className="user-author-img" src="https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/14876496_1735976403393352_4070401399338628514_o.jpg?oh=0ff4f0965c69809a048ea68c7dfb5836&oe=59C71556" alt=""/>
                     <h4>賴詰凱</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, in impedit ut dolores sint facere. Deserunt beatae voluptate natus architecto suscipit deleniti provident labore laboriosam! Sunt deserunt velit odit, sapiente.</p>
+                    <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, in impedit ut dolores sint facere. Deserunt beatae voluptate natus architecto suscipit deleniti provident labore laboriosam! Sunt deserunt velit odit, sapiente.</p>
+                    <Button>編輯</Button>
                 </div>
                 <div className="user-propose">
                     <h3>我主辦的工作坊</h3>
