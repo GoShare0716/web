ada52975df82550cf2e781170db42818195a1474
skyle0115 <skyle0115@gmail.com>
2017-07-23 22:05:51 +0800

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import {Provider} from 'react-redux';
+import {compose, createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
 import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import reducers from './reducers';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
