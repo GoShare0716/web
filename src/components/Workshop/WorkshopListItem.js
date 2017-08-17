38afa5464092a4de4d4e19c605dd0c7b4f7853e3
skyle0115 <skyle0115@gmail.com>
2017-08-17 12:01:00 +0800

    button to link, user editor

--- src/components/Workshop/WorkshopListItem.js
+++ src/components/Workshop/WorkshopListItem.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
-import moment from 'moment';
 import 'moment/locale/zh-tw';
-import {history} from '../../utils';
-import {Card, CardImg, CardBlock, Badge, Col} from 'reactstrap';
-import ProgressBar from '../Utils/ProgressBar';
-
 import './WorkshopListItem.css';
+
+import {Badge, Card, CardBlock, CardImg, Col} from 'reactstrap';
+import React, {Component} from 'react';
+
+import {Link} from 'react-router-dom';
+import ProgressBar from '../Utils/ProgressBar';
+import moment from 'moment';
+
+
+
+
+
 export default class WorkshopListItem extends Component {
     render() {
         let {
@@ -69,30 +75,32 @@ export default class WorkshopListItem extends Component {
             default:
 
         }
-
+        // onClick={e => history.push(`/workshop/${id}`)}
         return (
             <Col className="mb-3" xs={12} sm={6} lg={4}>
-                <Card className="workshop-list-item" onClick={e => history.push(`/workshop/${id}`)}>
-                    <div className="workshop-list-item-image-container">
-                        <CardImg top className="workshop-list-item-image" src={imageUrl} alt="card-image"/>
-                    </div>
-                    <Badge className="workshop-list-item-badge" color={badgeColor}>{badge}</Badge>
-                    <CardBlock>
-                        <h4 className="workshop-list-item-title mb-2">{title}</h4>
-                        <div className="text-muted mb-2">
-                            <span>{name}</span>
-                            <span className="mx-1">·</span>
-                            <span>{moment(startDatetime).format('M 月 D 日')}</span>
+                <Link to={`/workshop/${id}`} className="unlink">
+                    <Card className="workshop-list-item">
+                        <div className="workshop-list-item-image-container">
+                            <CardImg top className="workshop-list-item-image" src={imageUrl} alt="card-image"/>
                         </div>
-                        <ProgressBar className="mb-2" color={progressBarColor} value={progressBarValue} text={progressBarText}/>
-                        <div className="d-flex align-items-center justify-content-end">
-                            <span className="workshop-list-item-price-deleted mr-2">{priceDeletedText}</span>
-                            <span className="workshop-list-item-price" style={{
-                                color: priceColor
-                            }}>{priceText}</span>
-                        </div>
-                    </CardBlock>
-                </Card>
+                        <Badge className="workshop-list-item-badge" color={badgeColor}>{badge}</Badge>
+                        <CardBlock>
+                            <h4 className="workshop-list-item-title mb-2">{title}</h4>
+                            <div className="text-muted mb-2">
+                                <span>{name}</span>
+                                <span className="mx-1">·</span>
+                                <span>{moment(startDatetime).format('M 月 D 日')}</span>
+                            </div>
+                            <ProgressBar className="mb-2" color={progressBarColor} value={progressBarValue} text={progressBarText}/>
+                            <div className="d-flex align-items-center justify-content-end">
+                                <span className="workshop-list-item-price-deleted mr-2">{priceDeletedText}</span>
+                                <span className="workshop-list-item-price" style={{
+                                    color: priceColor
+                                }}>{priceText}</span>
+                            </div>
+                        </CardBlock>
+                    </Card>
+                </Link>
             </Col>
         );
     }
