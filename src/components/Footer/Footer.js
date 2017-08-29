9a368ec284d33b1cce00d614f3c21a578b9aeb2a
skyle0115 <skyle0115@gmail.com>
2017-08-29 12:44:41 +0800

    landing page col and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,14 +10,18 @@ class Footer extends Component {
 
     render() {
         return (
-            <footer className="mt-auto footer-container">
-                <div className="outer footer d-flex justify-content-between align-items-center">
+            <footer className="footer-container">
+                <div className="outer footer">
                     <small className="text-muted">© 2017 GoShare All Rights Reserved</small>
-                    <div>
-                        <Link to="/">關於我們</Link>
-                        <Link to="/workshop">看工作坊</Link>
-                        <Link to="/create-workshop">成為講者</Link>
-                        <a href="https://www.facebook.com/messages/t/goshare0716" target="_blank" rel="noopener noreferrer">聯絡我們</a>
+                    <div className="footer-anchor">
+                        <span>
+                            <Link to="/">關於我們</Link>
+                            <Link to="/workshop">看工作坊</Link>
+                        </span>
+                        <span>
+                            <Link to="/create-workshop">成為講者</Link>
+                            <a href="https://www.facebook.com/messages/t/goshare0716" target="_blank" rel="noopener noreferrer">聯絡我們</a>
+                        </span>
                         <a href="https://www.facebook.com/goshare0716/" target="_blank" rel="noopener noreferrer">
                             <i className="fa fa-facebook-square fa-lg" aria-hidden="true"/>
                         </a>
